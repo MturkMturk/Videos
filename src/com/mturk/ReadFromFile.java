b21d263e237b1f6bc96145e6068598118e6573d1
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2018-09-04 22:14:32 -0400

    Update ReadFromFile.java
    Changes

--- src/com/mturk/ReadFromFile.java
+++ src/com/mturk/ReadFromFile.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ReadFromFile
  */
-@WebServlet("/ReadFromFile")
+
 public class ReadFromFile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
