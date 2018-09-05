f97a4e5070bce711d39f3e0a4d5752991844502e
MturkMturk <31360631+MturkMturk@users.noreply.github.com>
2018-09-04 22:15:22 -0400

    Update WriteToFile.java
    Changes

--- src/com/mturk/WriteToFile.java
+++ src/com/mturk/WriteToFile.java
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class WriteToFile
  */
-@WebServlet("/WriteToFile")
 public class WriteToFile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
